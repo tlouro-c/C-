6677dd81294de2c181656d1c7f29fb6c08503013
Tomas Louro Correia <tlouro-c@c2r8s1.42lisboa.com>
2024-03-04 13:57:33 +0000

    Fix: CPP00 -> EX01

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -1,29 +1,32 @@
 #include "Contact.hpp"
 #include "PhoneBook.hpp"
 #include <cstdio>
+#include <cstdlib>
 
 
 int	main(void)
 {
 	PhoneBook	phonebook;
 	std::string	input;
-	int			index;
+	std::string	index;
 
 	while (input != "EXIT")
 	{
 		std::cout << std::endl<< "Use ADD, SEARCH or EXIT" << std::endl;
-		std::cin >> input;
+		std::getline(std::cin, input);
 		if (input == "ADD")
 			phonebook.AddContact();
 		else if (input == "SEARCH")
 		{
 			std::cout << phonebook;
 			std::cout << "Input an index: ";
-			std::cin >> index;
-			if (index > phonebook.GetTotalAddedContacts() || index > 8 || index < 1)
+			std::getline(std::cin, index);
+			if (std::atoi(index.c_str()) > phonebook.GetTotalAddedContacts() 
+				|| std::atoi(index.c_str()) > 8 
+				|| std::atoi(index.c_str()) < 1)
 				std::cerr << "Index out of range..." << std::endl;
 			else
-				std::cout << std::endl << phonebook.SearchByIndex(index) << std::endl;
+				std::cout << std::endl << phonebook.SearchByIndex(std::atoi(index.c_str())) << std::endl;
 		}
 	}
 	std::cout << "Bye :c" << std::endl;
