393fed64b122bc1334264153e2d1adbcd5a9a910
tlouro-c <tomascorreia.lc@gmail.com>
2024-03-06 15:41:18 +0000

    Fix: CPP01 -> EX03

--- CPP01/ex03/main.cpp
+++ CPP01/ex03/main.cpp
@@ -3,21 +3,21 @@
 #include "HumanB.hpp"
 
 int main() {
-  {
-    Weapon  club = Weapon("crude spiked club");
-    HumanA bob("Bob", club);
-    bob.attack();
-    club.setType("some other type of club");
-    bob.attack();
-   } 
-   {
-	Weapon club = Weapon("crude spkiked club");
-	HumanB jim("Jim");
-	jim.attack();
-	jim.setWeapon(club);
-	jim.attack();
-	club.setType("some other type of club");
-	jim.attack();
-   }
-return 0;
+	{
+		Weapon  club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	} 
+	{
+		Weapon club = Weapon("crude spkiked club");
+		HumanB jim("Jim");
+		jim.attack();
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return 0;
 }
