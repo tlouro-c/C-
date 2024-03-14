4172e4077d08db7af4c656fbc53f1a279dc05e2f
Tomas Louro Correia <tlouro-c@c2r2s6.42lisboa.com>
2024-03-14 18:13:13 +0000

    Progress: CPP06 -> EX01 ( main left to implement )

--- CPP06/ex01/Serializer.hpp
+++ CPP06/ex01/Serializer.hpp
@@ -1,9 +1,19 @@
 #ifndef SERIALIZER_HPP
 # define SERIALIZER_HPP
 
+#include <cstdint>
+
+struct Data
+{
+	char	*name;
+	char	*instructions;
+	int		numEntries;
+};
+
 class Serializer {
 public:
-	uintptr_t serialize(Data* ptr);
+	static uintptr_t serialize(Data* ptr);
+	static Data* deserialize(uintptr_t raw);
 private:
 	Serializer();
 	~Serializer();
