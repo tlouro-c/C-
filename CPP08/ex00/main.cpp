9cc5c8185f6c081ae4f7cf0478ba746da0b80832
Tomás Correia <142690383+tlouro-c@users.noreply.github.com>
2024-03-16 20:01:37 +0000

    Fix: CPP08 ( replaced “emplace” by “push”

--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -4,9 +4,9 @@ int	main(void)
 {
 	std::vector<int> intVector;
 
-	intVector.emplace_back(5);
-	intVector.emplace_back(2);
-	intVector.emplace_back(9);
+	intVector.push_back(5);
+	intVector.push_back(2);
+	intVector.push_back(9);
 	try
 	{
 		easyfind(intVector, 5);
